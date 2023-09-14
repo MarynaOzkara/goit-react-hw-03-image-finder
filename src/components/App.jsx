a0570aefefe685327bd8ebddffc2a004f2781bfd
MarynaOzkara <ozkaramaryna@gmail.com>
2023-09-14 22:39:59 +0300

    set api query

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,33 @@
 import { Component } from "react";
 import Searchbar from './Searchbar/Searchbar';
+import {getImages} from '../api/pixabay-api'
+
 export class App extends Component {
+  state = {
+      query: '',
+      page: 1,
+      images: [],
+      
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    const {query} = this.state;
+    getImages(query)
+    .then(({hits, total, totalHits}) => 
+      console.log(hits, totalHits, total))
+    .catch()
+  }
+  createSearchQuery = (searchQuery) =>{
+    console.log(searchQuery);
+    this.setState({
+      query: searchQuery, 
+    })
+
+  }
   render() {
     return (
       <>
-        <Searchbar/>
+        <Searchbar createSearchQuery={this.createSearchQuery}/>
 
         
       </>
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,12 +2,36 @@ import { Component } from "react";
 import css from './Searchbar.module.css';
 
 class Searchbar extends Component{
+  state = {
+     query: '',
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const {query} = this.state;
+    const searchQuery = query.trim();
+    if(!searchQuery){
+      return alert('Type your query to start serch images!');
+    }
+    this.props.createSearchQuery(searchQuery);
+    // console.log(searchQuery);
+    e.target.reset();
+  }
+  handleChange = ({currentTarget: {value}}) => {
+     this.setState({query: value})
+  }
     render(){
         return (
           <header className={css.header}>
-            <form>
-                <input type="text" />
-                <button type="submit">Search</button>
+            <form className={css.searchForm} onSubmit={this.handleSubmit}>
+                <input className={css.searchInput} onChange={this.handleChange} 
+                       type="text"
+                       value={this.query}
+                       autoComplete="off"
+                       autoFocus
+                       placeholder="Search images and photos" />
+                <button className={css.searchButton} type="submit">
+                  <span className={css.buttonLabel}>Search</span>
+                </button>
             </form>
           </header>
         )
